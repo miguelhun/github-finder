245b6f2a2b8cbd13646bc96799998561e3729b27
miguelhun <hunmiguel1124@gmail.com>
2020-04-06 15:46:22 -0500

    Github finder with React

---
+++ src/components/repos/Repos.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+import RepoItem from './RepoItem'
+
+const Repos = () => {
+	const githubContext = useContext(GithubContext)
+
+	return githubContext.repos.map(repo => <RepoItem repo={repo} key={repo.id} />)
+}
+
+export default Repos
